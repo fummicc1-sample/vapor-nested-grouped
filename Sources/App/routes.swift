fc92ad345afd511097daabaf33bc84c83d49c9c0
fummicc1 <fumiya.tennis1@gmail.com>
2024-05-03 18:11:43 +0900

    update

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,11 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+
+	app.grouped("foo").grouped("bar").get("", use: { _ in
+		"foo-bar"
+	})
+	app.grouped("foo/baz").get("", use: { _ in
+		"foo/baz"
+	})
 }
